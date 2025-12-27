9d8def549b29738b73df07f3306d426b1ff5a64d
gica <savamarius434@gmail.com>
2025-12-27 12:08:27 +0200

    c3

---
+++ c3.c
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+void main() {
+	
+	int a = 12;
+	int b = 34;
+
+	printf("%d %d %d", a & b, a ^ b, a | b);
+	
+	// char string[25];
+	// gets(string);
+	// puts(string);
+
+	// char c;
+	// c = getchar();
+	// while (c != '*') {
+	// 	putchar(c);
+	// 	c = getchar();
+	// }
+}
